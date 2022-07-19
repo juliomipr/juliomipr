5ef64ed7640d6b759a3fdc30998c941a6a8178b7
Julio Pozo <juliomipr@192.168.68.125>
2022-07-19 07:32:34 -0400

    updated gitignore

---
+++ lib/main_dev.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:matt_murdock/main.dart';
+
+void main() {
+  runApp(const MattMurdock(
+    appBarTitle: 'This is a portafolio build',
+    color: Colors.amber,
+    title: 'Demo App',
+  ));
+}
