95d8c55ccfc0f2945e3cc762d50449de81f12682
Julio Pozo <juliomipr@Julios-MacBook-Pro.local>
2022-07-26 12:30:22 -0400

    updated project list

--- lib/web_lib/widgets/portafolio.dart
+++ lib/web_lib/widgets/portafolio.dart
@@ -41,237 +41,235 @@ class _DesktopContent4State extends ConsumerState<DesktopContent4> {
       return 1200 / (factors.length);
     });
 
-    return Stack(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 48.0, left: 61.0, bottom: 20.0),
-              child: Text(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
                 'Previous Projects',
                 style: customTextStyle.headlineStyle,
               ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomCard(
-                    'assets/image/foodie.png',
-                    align: const Alignment(0.2, 0.0),
-                    number: '01',
-                    heightCard: 700.0,
-                    name: 'Foodie',
-                    factor: factors[0],
-                    subTitle:
-                        'Foodie application is an application for users to order a restaurant and find out which restaurant is located. This application greatly facilitates mobility.',
-                    onEnter: () {
-                      setState(() {
-                        if (pressed) {
+              const SizedBox(
+                height: 30.0,
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomCard(
+                      'assets/images/image1.png',
+                      align: const Alignment(0.2, 0.0),
+                      number: '01',
+                      heightCard: 700.0,
+                      name: 'Crypto',
+                      factor: factors[0],
+                      subTitle:
+                          'Crypto application is a blockchain application where users can easily view their invested portfolios in cryptocurency and buy bitcoin and sell bitcoin easily.',
+                      onEnter: () {
+                        setState(() {
+                          if (pressed) {
+                            factors[0] = 0.7;
+                            factors[1] = 0.1;
+                            factors[2] = 0.1;
+                            factors[3] = 0.1;
+                          } else {
+                            factors[0] = activeFactor;
+                            factors[1] = inActiveFactor;
+                            factors[2] = inActiveFactor;
+                            factors[3] = inActiveFactor;
+                          }
+                        });
+                      },
+                      onExit: () {
+                        setState(() {
+                          factors[0] = 0.25;
+                          factors[1] = 0.25;
+                          factors[2] = 0.25;
+                          factors[3] = 0.25;
+                        });
+                      },
+                      onPressed: () {
+                        setState(() {
                           factors[0] = 0.7;
                           factors[1] = 0.1;
                           factors[2] = 0.1;
                           factors[3] = 0.1;
-                        } else {
-                          factors[0] = activeFactor;
+                          pressed = true;
+                        });
+                      },
+                      onClosePressed: () {
+                        setState(() {
+                          factors[0] = inActiveFactor;
                           factors[1] = inActiveFactor;
                           factors[2] = inActiveFactor;
                           factors[3] = inActiveFactor;
-                        }
-                      });
-                    },
-                    onExit: () {
-                      setState(() {
-                        factors[0] = 0.25;
-                        factors[1] = 0.25;
-                        factors[2] = 0.25;
-                        factors[3] = 0.25;
-                      });
-                    },
-                    onPressed: () {
-                      setState(() {
-                        factors[0] = 0.7;
-                        factors[1] = 0.1;
-                        factors[2] = 0.1;
-                        factors[3] = 0.1;
-                        pressed = true;
-                      });
-                    },
-                    onClosePressed: () {
-                      setState(() {
-                        factors[0] = inActiveFactor;
-                        factors[1] = inActiveFactor;
-                        factors[2] = inActiveFactor;
-                        factors[3] = inActiveFactor;
-                        pressed = false;
-                      });
-                    },
-                  ),
-                  CustomCard(
-                    'assets/image/grocery.png',
-                    align: const Alignment(-0.1, 0.0),
-                    number: '02',
-                    name: 'Grocery',
-                    heightCard: 700.0,
-                    factor: factors[1],
-                    subTitle:
-                        'Grocery application is an application that makes it easy for users to buy basic necessities such as vegetables anywhere, anytime.',
-                    onEnter: () {
-                      setState(() {
-                        if (pressed) {
+                          pressed = false;
+                        });
+                      },
+                    ),
+                    CustomCard(
+                      'assets/images/image2.png',
+                      align: const Alignment(-0.1, 0.0),
+                      number: '02',
+                      name: 'E-Comerce',
+                      heightCard: 700.0,
+                      factor: factors[1],
+                      subTitle:
+                          'E-Comerce application is an application that allows you to buy merchandise from different vendors',
+                      onEnter: () {
+                        setState(() {
+                          if (pressed) {
+                            factors[0] = 0.1;
+                            factors[1] = 0.7;
+                            factors[2] = 0.1;
+                            factors[3] = 0.1;
+                          } else {
+                            factors[0] = inActiveFactor;
+                            factors[1] = activeFactor;
+                            factors[2] = inActiveFactor;
+                            factors[3] = inActiveFactor;
+                          }
+                        });
+                      },
+                      onExit: () {
+                        setState(() {
+                          factors[0] = 0.25;
+                          factors[1] = 0.25;
+                          factors[2] = 0.25;
+                          factors[3] = 0.25;
+                        });
+                      },
+                      onPressed: () {
+                        setState(() {
                           factors[0] = 0.1;
                           factors[1] = 0.7;
                           factors[2] = 0.1;
                           factors[3] = 0.1;
-                        } else {
+                          pressed = true;
+                        });
+                      },
+                      onClosePressed: () {
+                        setState(() {
                           factors[0] = inActiveFactor;
-                          factors[1] = activeFactor;
+                          factors[1] = inActiveFactor;
                           factors[2] = inActiveFactor;
                           factors[3] = inActiveFactor;
-                        }
-                      });
-                    },
-                    onExit: () {
-                      setState(() {
-                        factors[0] = 0.25;
-                        factors[1] = 0.25;
-                        factors[2] = 0.25;
-                        factors[3] = 0.25;
-                      });
-                    },
-                    onPressed: () {
-                      setState(() {
-                        factors[0] = 0.1;
-                        factors[1] = 0.7;
-                        factors[2] = 0.1;
-                        factors[3] = 0.1;
-                        pressed = true;
-                      });
-                    },
-                    onClosePressed: () {
-                      setState(() {
-                        factors[0] = inActiveFactor;
-                        factors[1] = inActiveFactor;
-                        factors[2] = inActiveFactor;
-                        factors[3] = inActiveFactor;
-                        pressed = false;
-                      });
-                    },
-                  ),
-                  CustomCard(
-                    'assets/image/crypto.png',
-                    align: const Alignment(-0.1, 0.0),
-                    number: '03',
-                    name: 'Crypto',
-                    heightCard: 700.0,
-                    factor: factors[2],
-                    subTitle:
-                        'Crypto application is a blockchain application where users can easily view their invested portfolios in cryptocurency and buy bitcoin and sell bitcoin easily.',
-                    onEnter: () {
-                      setState(() {
-                        if (pressed) {
+                          pressed = false;
+                        });
+                      },
+                    ),
+                    CustomCard(
+                      'assets/images/image3.png',
+                      align: const Alignment(-0.1, 0.0),
+                      number: '03',
+                      name: 'Trivia',
+                      heightCard: 700.0,
+                      factor: factors[2],
+                      subTitle:
+                          'Trivia is an app to play diferents trivia games.',
+                      onEnter: () {
+                        setState(() {
+                          if (pressed) {
+                            factors[0] = 0.1;
+                            factors[1] = 0.1;
+                            factors[2] = 0.7;
+                            factors[3] = 0.1;
+                          } else {
+                            factors[0] = inActiveFactor;
+                            factors[1] = inActiveFactor;
+                            factors[2] = activeFactor;
+                            factors[3] = inActiveFactor;
+                          }
+                        });
+                      },
+                      onExit: () {
+                        setState(() {
+                          factors[0] = 0.25;
+                          factors[1] = 0.25;
+                          factors[2] = 0.25;
+                          factors[3] = 0.25;
+                        });
+                      },
+                      onPressed: () {
+                        setState(() {
                           factors[0] = 0.1;
                           factors[1] = 0.1;
                           factors[2] = 0.7;
                           factors[3] = 0.1;
-                        } else {
+                          pressed = true;
+                        });
+                      },
+                      onClosePressed: () {
+                        setState(() {
                           factors[0] = inActiveFactor;
                           factors[1] = inActiveFactor;
-                          factors[2] = activeFactor;
+                          factors[2] = inActiveFactor;
                           factors[3] = inActiveFactor;
-                        }
-                      });
-                    },
-                    onExit: () {
-                      setState(() {
-                        factors[0] = 0.25;
-                        factors[1] = 0.25;
-                        factors[2] = 0.25;
-                        factors[3] = 0.25;
-                      });
-                    },
-                    onPressed: () {
-                      setState(() {
-                        factors[0] = 0.1;
-                        factors[1] = 0.1;
-                        factors[2] = 0.7;
-                        factors[3] = 0.1;
-                        pressed = true;
-                      });
-                    },
-                    onClosePressed: () {
-                      setState(() {
-                        factors[0] = inActiveFactor;
-                        factors[1] = inActiveFactor;
-                        factors[2] = inActiveFactor;
-                        factors[3] = inActiveFactor;
-                        pressed = false;
-                      });
-                    },
-                  ),
-                  CustomCard(
-                    'assets/image/recipes.png',
-                    align: const Alignment(0.0, 0.0),
-                    number: '04',
-                    name: 'Recipes',
-                    heightCard: 700.0,
-                    factor: factors[3],
-                    subTitle:
-                        'Recipe application is a food recipe application where users can easily view the recipes provided easily and view video recipe videos easily and precisely.',
-                    onEnter: () {
-                      setState(() {
-                        if (pressed) {
+                          pressed = false;
+                        });
+                      },
+                    ),
+                    CustomCard(
+                      'assets/images/image4.png',
+                      align: const Alignment(0.0, 0.0),
+                      number: '04',
+                      name: 'Weather',
+                      heightCard: 700.0,
+                      factor: factors[3],
+                      subTitle:
+                          'Weather application is a weather tracking app so you can have the weather for certain or current location',
+                      onEnter: () {
+                        setState(() {
+                          if (pressed) {
+                            factors[0] = 0.1;
+                            factors[1] = 0.1;
+                            factors[2] = 0.1;
+                            factors[3] = 0.7;
+                          } else {
+                            factors[0] = inActiveFactor;
+                            factors[1] = inActiveFactor;
+                            factors[2] = inActiveFactor;
+                            factors[3] = activeFactor;
+                          }
+                        });
+                      },
+                      onExit: () {
+                        setState(() {
+                          factors[0] = 0.25;
+                          factors[1] = 0.25;
+                          factors[2] = 0.25;
+                          factors[3] = 0.25;
+                        });
+                      },
+                      onPressed: () {
+                        setState(() {
                           factors[0] = 0.1;
                           factors[1] = 0.1;
                           factors[2] = 0.1;
                           factors[3] = 0.7;
-                        } else {
+                          pressed = true;
+                        });
+                      },
+                      onClosePressed: () {
+                        setState(() {
                           factors[0] = inActiveFactor;
                           factors[1] = inActiveFactor;
                           factors[2] = inActiveFactor;
-                          factors[3] = activeFactor;
-                        }
-                      });
-                    },
-                    onExit: () {
-                      setState(() {
-                        factors[0] = 0.25;
-                        factors[1] = 0.25;
-                        factors[2] = 0.25;
-                        factors[3] = 0.25;
-                      });
-                    },
-                    onPressed: () {
-                      setState(() {
-                        factors[0] = 0.1;
-                        factors[1] = 0.1;
-                        factors[2] = 0.1;
-                        factors[3] = 0.7;
-                        pressed = true;
-                      });
-                    },
-                    onClosePressed: () {
-                      setState(() {
-                        factors[0] = inActiveFactor;
-                        factors[1] = inActiveFactor;
-                        factors[2] = inActiveFactor;
-                        factors[3] = inActiveFactor;
-                        pressed = false;
-                      });
-                    },
-                  ),
-                ],
+                          factors[3] = inActiveFactor;
+                          pressed = false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
-      ],
-    );
+            ],
+          )
+        ],
+      );
+    });
   }
 }
